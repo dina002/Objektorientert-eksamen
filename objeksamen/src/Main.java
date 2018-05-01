9c0ff72140aa39885684572cbe1bcf90d9cbd649
dina002 <torstensen.simon@gmail.com>
2018-05-01 14:48:00 +0200

    Litt endringer i Main

--- objeksamen/src/Main.java
+++ objeksamen/src/Main.java
@@ -1,16 +1,26 @@
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 import gui.InputToStockfish;
 import gui.SjakkGUI;
 import server.Server;
 
-public class Main {
+public class Main implements Serializable{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		Server server = new Server();
 		//Board board = new Board();
-		new SjakkGUI("User one");
+		SjakkGUI s = new SjakkGUI("User one");
 		new SjakkGUI("User two");
 		//new InputToStockfish();
+		
+		ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("Test.dat"));
+        output.writeObject(s);
+        output.close();
+		
 	}
 
 }
